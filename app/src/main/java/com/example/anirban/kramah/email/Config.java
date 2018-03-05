5a31f1c8709bc5b7082ddf4ca9d26bb2d622476b
Kramah-blr <kramah.software.blr@gmail.com>
2018-03-05 16:31:02 +0530

    Mar 5 2nd Commit

--- app/src/main/java/com/example/anirban/kramah/email/Config.java
+++ app/src/main/java/com/example/anirban/kramah/email/Config.java
@@ -5,8 +5,7 @@ package com.example.anirban.kramah.email;
  */
 
 public class Config {
-//    public static final String EMAIL ="kramah.software.blr@gmail.com";
-//    public static final String PASSWORD ="*********";
-public static final String EMAIL ="incrediblebus8@gmail.com";
-    public static final String PASSWORD ="9046814817";
+    public static final String EMAIL ="kramah.software.blr@gmail.com";
+    public static final String PASSWORD ="********";
+
 }
